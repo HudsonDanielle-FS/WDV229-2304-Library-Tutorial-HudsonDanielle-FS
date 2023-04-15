2e6f3efe529df4a74d026597e7fed70bcc31f77e
HudsonDanielle-FS <110405791+HudsonDanielle-FS@users.noreply.github.com>
2023-04-15 09:44:34 -0500

    axios get request created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const getPkm = () => {
+    axios.get("GET https://pokeapi.co/api/v2/pokemon/ditto")
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section style={styles.cont}>
+      <button onClick={getPkm} style={styles.button}>
+        Give Me A Pokemon
+      </button>
+    </section>
   );
 }
 
 export default App;
+
+const styles = {
+  cont: {
+    backgroundColor: "#808080",
+    height: "100vh",
+  },
+  button: {
+    marginLeft: "45%",
+    marginTop: "10%",
+  },
+};
